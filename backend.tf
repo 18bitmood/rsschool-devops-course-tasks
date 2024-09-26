83197f1e172db18e4fcfc44e7d3a57df30bd3127
18bitmood <ivan.papushoi@mail.ru>
2024-09-26 23:53:15 +0300

    Add bucket ref

--- backend.tf
+++ backend.tf
@@ -6,3 +6,28 @@ terraform {
     encrypt = true
   }
 }
+
+data "aws_s3_bucket" "existing_bucket" {
+  bucket = "terraform-devops---rssc" # Reference your existing bucket name
+}
+
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = data.aws_s3_bucket.existing_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::145023129007:role/GithubActionsRole"
+        }
+        Action = "s3:*"
+        Resource = [
+          data.aws_s3_bucket.existing_bucket.arn,
+          "${data.aws_s3_bucket.existing_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
