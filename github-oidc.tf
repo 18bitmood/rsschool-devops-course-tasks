e52e446b7cd00079496bb2e53ac447385c24cf02
18bitmood <ivan.papushoi@mail.ru>
2024-09-29 09:04:18 +0300

    Add wildcard

--- github-oidc.tf
+++ github-oidc.tf
@@ -26,7 +26,8 @@ data "aws_iam_policy_document" "github_assume_role_policy" {
       variable = "token.actions.githubusercontent.com:sub"
       values = [
         "repo:18bitmood/rsschool-devops-course-tasks:ref:refs/heads/main",
-        "repo:18bitmood/rsschool-devops-course-tasks:ref:refs/heads/task_1_submission"
+        "repo:18bitmood/rsschool-devops-course-tasks:ref:refs/heads/task_1_submission",
+        "repo:18bitmood/rsschool-devops-course-tasks:ref:refs/heads/*"
       ]
     }
   }
