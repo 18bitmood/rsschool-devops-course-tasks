bbeb7dfbbec9421c1b31e3665bebbc92eef7ffd8
18bitmood <ivan.papushoi@mail.ru>
2024-10-19 15:45:58 +0300

    Move cluster to private net

--- bastion.tf
+++ bastion.tf
@@ -29,26 +29,25 @@ resource "aws_instance" "bastion" {
       "chown ubuntu:ubuntu /home/ubuntu/.ssh/${var.bastion_key_name}.pem",
 
       # Install k3s on the first public instance
-      "ssh -i /home/ubuntu/.ssh/${var.bastion_key_name}.pem -o StrictHostKeyChecking=no ubuntu@${aws_instance.ubuntu_public_instances[0].private_ip} 'curl -sfL https://get.k3s.io -o k3s-install.sh && sudo sh k3s-install.sh'",
-      "sleep 120",
+      "ssh -i /home/ubuntu/.ssh/${var.bastion_key_name}.pem -o StrictHostKeyChecking=no ubuntu@${aws_instance.ubuntu_private_instances[0].private_ip} 'curl -sfL https://get.k3s.io -o k3s-install.sh && sudo sh k3s-install.sh'",
+      "sleep 10",
 
       # Retrieve the node token and save it to a file
-      "ssh -i /home/ubuntu/.ssh/${var.bastion_key_name}.pem -o StrictHostKeyChecking=no ubuntu@${aws_instance.ubuntu_public_instances[0].private_ip} 'sudo cat /var/lib/rancher/k3s/server/node-token' > /home/ubuntu/k3s_token.txt",
+      "ssh -i /home/ubuntu/.ssh/${var.bastion_key_name}.pem -o StrictHostKeyChecking=no ubuntu@${aws_instance.ubuntu_private_instances[0].private_ip} 'sudo cat /var/lib/rancher/k3s/server/node-token' > /home/ubuntu/k3s_token.txt",
 
       # Install k3s in agent mode on other public instances
       "${join("\n",
         flatten([
-          for ip in slice(aws_instance.ubuntu_public_instances[*].private_ip, 1, length(aws_instance.ubuntu_public_instances)) : [
+          for ip in slice(aws_instance.ubuntu_private_instances[*].private_ip, 1, length(aws_instance.ubuntu_private_instances)) : [
             # Transfer the node token to the agent instances
             "scp -i /home/ubuntu/.ssh/${var.bastion_key_name}.pem -o StrictHostKeyChecking=no /home/ubuntu/k3s_token.txt ubuntu@${ip}:/home/ubuntu/k3s_token.txt",
             # Download the k3s installer script
             "ssh -i /home/ubuntu/.ssh/${var.bastion_key_name}.pem -o StrictHostKeyChecking=no ubuntu@${ip} 'curl -sfL https://get.k3s.io -o k3s-install.sh'",
             # Install k3s in agent mode
-            "ssh -i /home/ubuntu/.ssh/${var.bastion_key_name}.pem -o StrictHostKeyChecking=no ubuntu@${ip} 'K3S_URL=https://${aws_instance.ubuntu_public_instances[0].private_ip}:6443 K3S_TOKEN=$(cat /home/ubuntu/k3s_token.txt) sh k3s-install.sh agent &'"
+            "ssh -i /home/ubuntu/.ssh/${var.bastion_key_name}.pem -o StrictHostKeyChecking=no ubuntu@${ip} 'timeout 300 bash -c \"K3S_URL=https://${aws_instance.ubuntu_private_instances[0].private_ip}:6443 K3S_TOKEN=$(cat /home/ubuntu/k3s_token.txt) sh k3s-install.sh agent\"'"
           ]
         ])
-      )}",
-      "sleep 150"
+      )}"
     ]
 
     connection {
